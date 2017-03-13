c79bd2d329a36efaaaebb10f44bab5e12ba3dc6a
roxma <roxma@qq.com>
2017-03-13 15:35:57 +0800

    fmt remove boost

--- samples/dep/make/fmt.module.mk
+++ samples/dep/make/fmt.module.mk
@@ -4,7 +4,7 @@ all: $(PREFIX)/include/fmt/format.h
 $(PREFIX)/include/fmt/format.h:
 
 	# If this library has more dependencies
-	$(call DEP_MAKE,boost.module.mk)
+	# $(call DEP_MAKE,boost.module.mk)
 
 	$(call DEP_DOWNLOAD,https://github.com/fmtlib/fmt/archive/3.0.1.tar.gz,3.0.1.tar.gz,md5sum=b2c97427a696182b013d2cc0a2f939fe)
 	tar -zxf 3.0.1.tar.gz
