1572f3ac615663f9bacb59b5e1105248990b535b
roxma <roxma@qq.com>
2022-07-28 15:16:06 +0800

    Remove useless easymake_dep.mk samples

--- samples/dep/make/boost.module.mk
+++
@@ -1,9 +0,0 @@
-
-all: $(PREFIX)/include/boost/lambda/lambda.hpp
-
-$(PREFIX)/include/boost/lambda/lambda.hpp:
-	$(call DEP_DOWNLOAD,https://nchc.dl.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz,boost_1_63_0.tar.gz,md5sum=7b493c08bc9557bbde7e29091f28b605)
-	rm -rf boost_1_63_0
-	tar -zxf boost_1_63_0.tar.gz
-	cd boost_1_63_0 && ./bootstrap.sh --prefix=$(PREFIX) && ./b2 install
-
--- samples/dep/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "fmt/format.h"
-
-int main(int argc, const char *argv[])
-{
-  fmt::print("Hello, {}!\n", "world");
-  return 0;
-}
-
